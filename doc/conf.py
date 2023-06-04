ac0719d7b1dbd9b02be6a27b3d21704db8987393
gormaniac <83027042+gormaniac@users.noreply.github.com>
2023-06-04 13:09:53 -0600

    feat: working clscidapi

---
+++ doc/conf.py
@@ -0,0 +1,11 @@
+project = 'clscidapi'
+copyright = '2023, John Gorman'
+author = 'John Gorman'
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = []
